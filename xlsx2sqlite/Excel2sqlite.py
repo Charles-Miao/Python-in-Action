2865e6b2e2f5ddaf848f83ee6268e8515016ba8d
charles-miao <fengkuang33@163.com>
2017-04-11 23:37:56 +0800

    update it

--- xlsx2sqlite/Excel2sqlite.py
+++ xlsx2sqlite/Excel2sqlite.py
@@ -48,6 +48,8 @@ def createDataBase(cn):
 
 	
 def readExcel(filename,cn):
+	ess=sqlite3.connect(cn)
+	
 	workbook = xlrd.open_workbook(filename)
 	sheet_name = workbook.sheet_names()[0]
 	sheet = workbook.sheet_by_name(sheet_name)
@@ -60,15 +62,30 @@ def readExcel(filename,cn):
 				temp.append(sheet.cell(i,j).value)
 			except (AttributeError):
 				temp.append(None)
-		#print(temp[1])
+		#print(temp[39])
 		
-		cn.execute('''insert into ALL_ESS (PLANT,ESS_NUMBER,IMPROVEMENT_SUBJECT,PROPOSAL_DATE,PROPOSAL_DEPARTMENT,PROPOSER,PROPOSER_NUMBER,APPROVED,STATUS,ACCEPTED_DAYS,ACCEPTANCE_TIME,OFFICER_ACCEPTANCE_TIME,EXECUTOR_NUMBER,EXECUTOR,ASSIGN_PIC_PLANT,COMPLETION_DATA,EXECUTOR_DEPARTMENT,ESTIMATED_MANPOWER,ESTIMATED_BENEFIT,ACTUAL_MANPOWER,ACTUAL_BENEFIT,ESTIMATED_STARTDAY,ESTIMATED_DUEDAY,WHETHER_PAYMENT,MODIFIED_TIME,SEND_MAIL_TO_APPROVER,COMPLETE_DATE,IMPROVE_APPROVER_NAME,SITE_ID,PADEPT_ID,VAR_ASSIGN_PICDEPT_ID,VAR_PADEPT_MANAGER_ID,MODIFIER,FLOW_STEP_START_DATE,YEAR,SIGN_STEP,FOUNDER,ITEM_TYPE,PATH,DATA) values (temp[0],temp[1],temp[2],temp[3],temp[4],temp[5],temp[6],temp[7],temp[8],temp[9],temp[10],temp[11],temp[12],temp[13],temp[14],temp[15],temp[16],temp[17],temp[18],temp[19],temp[20],temp[21],temp[22],temp[23],temp[24],temp[25],temp[26],temp[27],temp[28],temp[29],temp[30],temp[31],temp[32],temp[33],temp[34],temp[35],temp[36],temp[37],temp[38],temp[39]);''')
-	cn.commit()
+		ess.execute('''INSERT INTO ALL_ESS 
+		(PLANT,ESS_NUMBER,IMPROVEMENT_SUBJECT,PROPOSAL_DATE,PROPOSAL_DEPARTMENT,
+		PROPOSER,PROPOSER_NUMBER,APPROVED,STATUS,ACCEPTED_DAYS,
+		ACCEPTANCE_TIME,OFFICER_ACCEPTANCE_TIME,EXECUTOR_NUMBER,EXECUTOR,ASSIGN_PIC_PLANT,
+		COMPLETION_DATA,EXECUTOR_DEPARTMENT,ESTIMATED_MANPOWER,ESTIMATED_BENEFIT,ACTUAL_MANPOWER,
+		ACTUAL_BENEFIT,ESTIMATED_STARTDAY,ESTIMATED_DUEDAY,WHETHER_PAYMENT,MODIFIED_TIME,
+		SEND_MAIL_TO_APPROVER,COMPLETE_DATE,IMPROVE_APPROVER_NAME,SITE_ID,PADEPT_ID,
+		VAR_ASSIGN_PICDEPT_ID,VAR_PADEPT_MANAGER_ID,MODIFIER,FLOW_STEP_START_DATE,YEAR,
+		SIGN_STEP,FOUNDER,ITEM_TYPE,PATH,DATA) 
+		VALUES (temp[0],temp[1],temp[2],temp[3],temp[4],temp[5],
+		temp[6],temp[7],temp[8],temp[9],temp[10],
+		temp[11],temp[12],temp[13],temp[14],temp[15],
+		temp[16],temp[17],temp[18],temp[19],temp[20],
+		temp[21],temp[22],temp[23],temp[24],temp[25],
+		temp[26],temp[27],temp[28],temp[29],temp[30],
+		temp[31],temp[32],temp[33],temp[34],temp[35],
+		temp[36],temp[37],temp[38],temp[39]);''')
+	ess.commit()
 
 
 if __name__=='__main__':
 	filename="活頁簿1.xlsx"
 	cn="ess.db"
 	createDataBase(cn)
-	database = sqlite3.connect(cn)
-	readExcel(filename,database)
+	readExcel(filename,cn)
--- xlsx2sqlite/README.md
+++ xlsx2sqlite/README.md
@@ -13,3 +13,21 @@
 **[【RUNOOB.COM】SQLite - Python](http://www.runoob.com/sqlite/sqlite-python.html)**
 
 [【10条】Python批量Excel文件数据导入SQLite数据库的优化方案](http://www.10tiao.com/html/383/201702/2247484174/1.html)
+
+http://stackoverflow.com/questions/5203703/sqlite3-operationalerror-near-x-syntax-error
+
+http://www.cnblogs.com/mmix2009/p/3225489.html
+
+http://www.aichengxu.com/python/11218174.htm
+
+http://www.itguai.com/python/a5017793.html
+
+https://lzps.github.io/2017/01/24/sql-study/
+
+https://github.com/linmonsv/python3/blob/master/Python%2020%20%E8%AE%BF%E9%97%AE%E6%95%B0%E6%8D%AE%E5%BA%93-SQLite.py
+
+http://www.open-open.com/code/view/1420378728062
+
+http://www.itkeyword.com/doc/2419194120317778546/how-can-i-create-a-database-using-sqlite3-in-python-3-for-mac
+
+https://segmentfault.com/q/1010000005939017
