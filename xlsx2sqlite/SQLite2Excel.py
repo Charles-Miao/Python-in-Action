4a520b0f0c9d96316fc56afc23e42856ae6b1e03
charles-miao <fengkuang33@163.com>
2017-04-13 20:13:41 +0800

    1. 添加两个表用于暂存被提案部门为MEZ900和提案部门为MEZ900的提案 2. 完成5个功能： 功能1：本月提案，并被厂长Approve 功能2：本月有节省人力的提案 功能3：正在进行中的提案 功能4：结案嘉奖提案 功能5：提案嘉奖提案

--- xlsx2sqlite/SQLite2Excel.py
+++ xlsx2sqlite/SQLite2Excel.py
@@ -1,24 +1,72 @@
 import sqlite3
-
-ess=sqlite3.connect("ess.db")
-#cursor=ess.execute("select * from ALL_ESS where PROPOSAL_DEPARTMENT='MEZ910'")
-#for row in cursor:
-#	print(row)
-
 from datetime import date,datetime
-#获取今天日期，并转换为年月格式
-now=date.today()
-nowmonth=now.strftime('%Y-%m')
-#striptime：将Str转换为Date格式
-cursor=ess.execute("select PROPOSAL_DATE from ALL_ESS")
-for row in cursor:
+
+if __name__=='__main__':
+	#初始化
+	cn="ess.db"
+	ess=sqlite3.connect("ess.db")	
 	
-	try:
-		datetmp=datetime.strptime(row[0],'%Y-%m-%d')
-	except:
-		datetmp=datetime.strptime('1900-12-21','%Y-%m-%d')
-	if datetmp.strftime('%Y-%m')==nowmonth:
-		print(row[0])
-
-
+	#获取今天日期，并转换为（年-月）格式
+	now=date.today()
+	nowmonth=now.strftime('%Y-%m')
+	
+	'''
+	功能1：本月提案，并被厂长Approve
+	'''
+	#datetemp=datetime.strptime('2017-03-21','%Y-%m-%d')
+	#lastmonth=datetemp.strftime('%Y-%m')
+	#striptime：将Str转换为Date格式
+	cursor=ess.execute("select * from PROPOSAL_DEPARTMENT where STATUS='Close' or STATUS='Implement'")
+	#row[3]：提案日期
+	for row in cursor:
+		#print(row)
+		try:
+			datetmp=datetime.strptime(row[3],'%Y-%m-%d')
+		except:
+			datetmp=datetime.strptime('1900-12-21','%Y-%m-%d')
+		if datetmp.strftime('%Y-%m')==nowmonth:
+			print(row)
+	
+	
+	'''
+	功能2：本月有节省人力的提案
+	'''
+	#datetemp=datetime.strptime('2017-03-21','%Y-%m-%d')
+	#lastmonth=datetemp.strftime('%Y-%m')
+	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Close' and ACTUAL_MANPOWER IS NOT NULL and ACTUAL_MANPOWER!=0.0")
+	#row[15]:完成日期
+	for row in cursor:
+		#print(row)
+		try:
+			datetmp=datetime.strptime(row[15],'%Y-%m-%d')
+		except:
+			datetmp=datetime.strptime('1900-12-21','%Y-%m-%d')
+		if datetmp.strftime('%Y-%m')==nowmonth:
+			print(row)
+	
+	
+	'''
+	功能3：正在进行中的提案
+	'''
+	#ASC，升序
+	#DESC，降序
+	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Issue' or STATUS='Implement' order by STATUS DESC, ESTIMATED_DUEDAY ASC")
+	#for row in cursor:
+	#	print(row)
+		
+	
+	'''
+	功能4：结案嘉奖提案
+	'''
+	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Close' and ACTUAL_BENEFIT>=2000 order by COMPLETION_DATE DESC")
+	#for row in cursor:
+	#	print(row)
+	
+	
+	'''
+	功能5：提案嘉奖提案
+	'''
+	cursor=ess.execute("select * from PROPOSAL_DEPARTMENT where STATUS='Close' or STATUS='Implement' order by PROPOSAL_DATE DESC")
+	#for row in cursor:
+	#	print(row)
 	
