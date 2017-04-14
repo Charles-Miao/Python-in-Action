656b83f423bef1e2d630acda300258ac4c5c0085
charles-miao <fengkuang33@163.com>
2017-04-14 11:25:15 +0800

    Version2.1 1. 添加插入表头功能

--- xlsx2sqlite/SQLite2Excel.py
+++ xlsx2sqlite/SQLite2Excel.py
@@ -9,6 +9,20 @@ def createExcel(filename):
 	book=xlwt.Workbook(encoding='utf-8',style_compression=0)
 	sheet=book.add_sheet("sheet1",cell_overwrite_ok=True)
 	book.save(filename)	
+
+
+#将筛选数据库的结果，塞入进EXCEL（插入表头）
+def insertExcel(cursor,excel):
+	row_number=0
+	for row in cursor:
+		#print(row)
+		rb=xlrd.open_workbook(excel)  
+		wb=copy(rb)  
+		ws=wb.get_sheet(0)  
+		for i in range(40):
+			ws.write(row_number,i,row[i]) 
+		wb.save(excel)
+		row_number=row_number+1
 	
 	
 if __name__=='__main__':
@@ -17,7 +31,7 @@ if __name__=='__main__':
 	ess=sqlite3.connect("ess.db")	
 	
 	excel1="01本月提案.xls"
-	excel2="02本月剩人提案.xls"
+	excel2="02本月省人提案.xls"
 	excel3="03正在进行中提案.xls"
 	excel4="04结案嘉奖提案.xls"
 	excel5="05提案嘉奖提案.xls"
@@ -48,6 +62,19 @@ if __name__=='__main__':
 	#获取今天日期，并转换为（年-月）格式
 	now=date.today()
 	nowmonth=now.strftime('%Y-%m')
+	'''
+	功能0：插入表头
+	'''
+	cursor=ess.execute("select * from ALL_ESS where PLANT='廠別'")
+	insertExcel(cursor,excel1)
+	cursor=ess.execute("select * from ALL_ESS where PLANT='廠別'")
+	insertExcel(cursor,excel2)
+	cursor=ess.execute("select * from ALL_ESS where PLANT='廠別'")
+	insertExcel(cursor,excel3)
+	cursor=ess.execute("select * from ALL_ESS where PLANT='廠別'")
+	insertExcel(cursor,excel4)
+	cursor=ess.execute("select * from ALL_ESS where PLANT='廠別'")
+	insertExcel(cursor,excel5)
 	
 	'''
 	功能1：本月提案，并被厂长Approve
@@ -57,7 +84,7 @@ if __name__=='__main__':
 	#striptime：将Str转换为Date格式
 	cursor=ess.execute("select * from PROPOSAL_DEPARTMENT where STATUS='Close' or STATUS='Implement'")
 	#row[3]：提案日期
-	row_number=0
+	row_number=1
 	for row in cursor:
 		#print(row)
 		try:
@@ -81,7 +108,7 @@ if __name__=='__main__':
 	#lastmonth=datetemp.strftime('%Y-%m')
 	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Close' and ACTUAL_MANPOWER IS NOT NULL and ACTUAL_MANPOWER!=0")
 	#row[15]:完成日期
-	row_number=0
+	row_number=1
 	for row in cursor:
 		#print(row)
 		try:
@@ -104,7 +131,7 @@ if __name__=='__main__':
 	#ASC，升序
 	#DESC，降序
 	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Issue' or STATUS='Implement' order by STATUS DESC, ESTIMATED_DUEDAY ASC")
-	row_number=0
+	row_number=1
 	for row in cursor:
 		#print(row)
 		rb=xlrd.open_workbook(excel3)  
@@ -120,7 +147,7 @@ if __name__=='__main__':
 	功能4：结案嘉奖提案
 	'''
 	cursor=ess.execute("select * from EXECUTOR_DEPARTMENT where STATUS='Close' and ACTUAL_BENEFIT>=2000 order by COMPLETION_DATE DESC")
-	row_number=0
+	row_number=1
 	for row in cursor:
 		#print(row)
 		rb=xlrd.open_workbook(excel4)  
@@ -135,7 +162,7 @@ if __name__=='__main__':
 	功能5：提案嘉奖提案
 	'''
 	cursor=ess.execute("select * from PROPOSAL_DEPARTMENT where STATUS='Close' or STATUS='Implement' order by PROPOSAL_DATE DESC")
-	row_number=0
+	row_number=1
 	for row in cursor:
 		#print(row)
 		rb=xlrd.open_workbook(excel5)  
